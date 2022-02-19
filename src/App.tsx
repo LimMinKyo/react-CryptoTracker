1746b4016f2ab986feccae7c3cc777715b782fa9
LimMinKyo <story1400@naver.com>
2022-02-19 10:13:42 +0900

    Learn Typescript (2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import Circle from './Circle';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor};
+`;
 
 function App() {
+  const [value, setValue] = useState('');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // setValue(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Hello', value);
+  };
   return (
     <div>
-      <Circle bgColor='teal' borderColor='yellow'/>
-      <Circle text='Im here' bgColor='tomato' />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log In</button>
+      </form>
+      <Container>
+        <H1>Hello</H1>
+      </Container>
     </div>
   );
 }
