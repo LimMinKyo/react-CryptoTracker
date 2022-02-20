5c38a39ab090a54137622b648317c99806d1f255
LimMinKyo <story1400@naver.com>
2022-02-21 01:25:25 +0900

    Setup Crypto-Tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,7 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  background-color: ${props => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${props => props.theme.textColor};
-`;
+import Router from './Router';
 
 function App() {
-  const [value, setValue] = useState('');
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    // setValue(event.currentTarget.value);
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log('Hello', value);
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log In</button>
-      </form>
-      <Container>
-        <H1>Hello</H1>
-      </Container>
-    </div>
-  );
+  return <Router />
 }
 
 export default App;
